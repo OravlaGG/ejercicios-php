50097b970fc99e633f8620335376b33f2e3e08a2
alvarotorru <alvarotorru@gmail.com>
2025-10-24 18:00:24 +0200

    Se ha añadido el ejercicio 19 y 20 como uno solo

---
+++ relacion2/ejercicio19-20/formulario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 18 Relación 2 - Formulario Binario</title>
+    <link rel="shortcut icon" href="../img/playamar.png" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        .form-text
+        {
+            visibility: hidden;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Ejercicio 18 Relación 2 - Formulario Binario</h1>
+    </div>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+    <form action="resultado.php" id="formT" method="get"class="p-3 shadow rounded">
+    <div class="mb-3">
+        <label for="numero1" class="form-label">Introduce el numero</label>
+        <input type="text" name="numero1" id="numero1" class="form-control">
+        <div id="numero1Help" class="form-text text-danger">El número esta vacio o es menor a 0</div>
+    </div>
+    <div class="mb-3">
+        <label for="seleccion" class="form-label">Introduce una opcion</label>
+        <select name="seleccion" id="seleccion">
+            <option value="nada">Selecciona</option>
+            <option value="binario">Binario</option>
+            <option value="octal">Octal</option>
+            <option value="hexadecimal">Hexadecimal</option>
+
+        </select>
+    </div>
+    <input type="submit" value="Enviar">
+    </form>
+    </div>
+    <script src="./validacion.js"></script>
+
+</body>
+</html>
