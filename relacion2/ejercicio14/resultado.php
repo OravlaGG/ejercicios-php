5fd9ea861ece1727d05908deb7b4fb955fb9dde7
OravlaGG <alvarotorru@gmail.com>
2025-10-26 13:02:53 +0100

    Icono arreglado

--- relacion2/ejercicio14/resultado.php
+++ relacion2/ejercicio14/resultado.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 9 de la Relación 1 - TRIANGULOS</title>
-    <link rel="shortcut icon" href="../img/playamar.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../../img/playamar.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
