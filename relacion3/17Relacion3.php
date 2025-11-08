fa04adebde7bf1b3f248bab1a79be7a7c41a724d
OravlaGG <alvarotorru@gmail.com>
2025-11-08 17:32:46 +0100

    Ejercicio 17 Por Terminar

--- relacion3/17Relacion3.php
+++ relacion3/17Relacion3.php
@@ -3,18 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funciones CallBack</title>
+    <title>Funciones CallBackv2</title>
     <link rel="shortcut icon" href="../img/playamar.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
 </head>
 <body>
     <div class="container">
-        <h1>Ejercicio 16 Relación 3 - Funciones CallBack</h1>
+        <h1>Ejercicio 17 Relación 3 - Funciones CallBackv2</h1>
     </div>
 
     <?php
-        $arr = range(1,100);
+        $arr = range(1,20);
+        $pares = array_filter($arr,function ($n) {return ($n % 2 == 0);});
+
+        $arr = range(1,40);
+        $multiplosTres = array_filter($arr,function ($n) {return ($n % 3 == 0);});
         /*Esta no funciona. Funcionaria  de la siguiente manera creamos un do while
         que recorra el array cuya condicion de salida sea una flag que cambiaria si 
         encuentra un número que no es positivo*/
@@ -26,25 +30,12 @@
         que recorra el array cuya condicion de salida sea una flag que cambiaria si 
         encuentra un número que es divisible entre 5*/
         /*echo"<div class='alert alert-secondary' role='alert'>";
-        $tmp = array_all($arr, function ($n) { return ($n % 5 == 0);});
+        $tmp = array_any($arr, function ($n) { return ($n % 5 == 0);});
         echo "Son todos los numeros positivos? ";
         echo"</div>";*/
-        $primo = function ($n) 
+        $pares = function ($n) 
         { 
-            $div = 2;
-            $esPrim = true;
-
-            while($div < $n && $esPrim != false)
-            {
-                if ($n % $div == 0)
-                {
-                    $esPrim = false;
-                }
-
-                $div++;
-            }
-
-            return $esPrim;
+            return ($n % 2 == 0);
         };
         echo"<div class='alert alert-secondary' role='alert'>";
         $tmp = array_filter($arr, $primo);//Devuelve Almacena los elementos que son true
